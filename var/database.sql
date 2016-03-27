fb5a478e1fea99880b92a409c4602d2a077f6038
tyurd <tyurderi@yahoo.de>
2016-03-28 01:44:16 +0200

    update database structure

--- var/database.sql
+++ var/database.sql
@@ -22,3 +22,16 @@ CREATE TABLE `config_field` (
 ALTER TABLE `config_field`
   ADD CONSTRAINT `fk_config_field_config`
     FOREIGN KEY (`config_id`) REFERENCES `config` (`id`);
+
+CREATE TABLE `config_column` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `config_id` int(11) NOT NULL,
+  `config_field_id` int(11) NOT NULL,
+  PRIMARY KEY(`id`)
+);
+
+ALTER TABLE `config_column`
+  ADD CONSTRAINT `fk_config_column_config`
+    FOREIGN KEY (`config_id`) REFERENCES `config` (`id`),
+  ADD CONSTRAINT `fk_config_column_config_field`
+    FOREIGN KEY (`config_field_id`) REFERENCES `config_field` (`id`);
