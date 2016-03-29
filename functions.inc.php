4d120456e1e36858ca01791532734724b3cc7938
Tommy <meier@rhinos-media.de>
2016-03-29 12:02:40 +0200

    add string helper functions

---
+++ functions.inc.php
@@ -0,0 +1,32 @@
+<?php
+
+if(!function_exists('camelize'))
+{
+
+    function camelize($scored)
+    {
+        return ucfirst(
+            implode(
+                '',
+                array_map(
+                    'ucfirst',
+                    array_map(
+                        'strtolower',
+                        explode(
+                            '_', $scored)))));
+    }
+
+}
+
+if(!function_exists('underscore'))
+{
+
+    function underscore($cameled) {
+        return implode(
+            '_',
+            array_map(
+                'strtolower',
+                preg_split('/([A-Z]{1}[^A-Z]*)/', $cameled, -1, PREG_SPLIT_DELIM_CAPTURE|PREG_SPLIT_NO_EMPTY)));
+    }
+
+}
