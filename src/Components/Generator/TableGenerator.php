efc2e10374d64c7d50a0b857e817a8ec84890c4e
Tommy <meier@rhinos-media.de>
2016-03-29 13:58:59 +0200

    add table generator

---
+++ src/Components/Generator/TableGenerator.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace TM\Config\Components\Generator;
+
+use TM\Config\Field\Field;
+use TM\Config\Components\ComponentAbstract;
+use TM\Table\Field\Type;
+use TM\Table\Manager;
+use TM\Table\Table;
+
+class TableGenerator extends ComponentAbstract
+{
+
+    /**
+     * @var Manager
+     */
+    protected $manager;
+
+    public function initialize()
+    {
+        $this->manager = new Manager();
+    }
+
+    /**
+     * Generates a table out of a Configuration fields.
+     *
+     * @param string  $name
+     * @param Field[] $fields
+     *
+     * @return boolean
+     */
+    public function generate($name, $fields)
+    {
+        $table = new Table($name);
+
+        $table->addField('id', Type::int(11))->setAutoIncrement(true)->setPrimaryKey(true);
+
+        foreach($fields as $field)
+        {
+            $table->addField($field->getName(), $field->getType()->getDatabaseType());
+        }
+
+        $query = $this->manager->createQuery($table);
+
+        return $this->app->getDB()->query($query)->execute();
+    }
+
+    public function drop($name)
+    {
+        $query = 'DROP TABLE IF EXISTS `' . $name . '`';
+
+        return $this->app->getDB()->query($query)->execute();
+    }
+
+}
