500c310e60e6f573da3f5c93b8f04aed3e62575a
tyurd <tyurderi@yahoo.de>
2016-04-09 17:40:53 +0200

    better dispatching

--- src/Web/Application.php
+++ src/Web/Application.php
@@ -32,15 +32,15 @@ class Application extends \TM\Config\Application
     {
         list($controller, $action) = $this->parseTarget($target);
 
-        if(class_exists($controller) && method_exists($controller, $action))
+        if(class_exists($controller) && method_exists($this->slim, $method))
         {
             $slim = $this->slim;
 
-            $this->slim->map([$method], $route, function($request, $response, $params) use ($slim, $controller, $action) {
+            $this->slim->map(array($method), $route, function($request, $response, $params) use ($slim, $controller, $action) {
                 /** @var ControllerAbstract $controller */
                 $controller = new $controller($slim);
 
-                return $controller->{$action}($request, $response, $params);
+                return $controller->dispatch($action, $params);
             });
         }
     }
@@ -50,7 +50,6 @@ class Application extends \TM\Config\Application
         $result = explode('.', $target, 2);
 
         $result[0] = 'TM\\Config\\Web\\Controllers\\' . ucfirst($result[0]) . 'Controller';
-        $result[1] = $result[1] . 'Action';
 
         return $result;
     }
--- src/Web/ControllerAbstract.php
+++ src/Web/ControllerAbstract.php
@@ -5,14 +5,35 @@ namespace TM\Config\Web;
 abstract class ControllerAbstract
 {
 
+    /**
+     * @var \Slim\App
+     */
+    protected $slim;
+
     final public function __construct(\Slim\App $slim)
     {
+        $this->slim = $slim;
+
         $this->initialize();
     }
 
     protected function initialize()
     {
 
+    }
+
+    public function dispatch($action, $params = array())
+    {
+        $action = $action . 'Action';
+        if(method_exists($this, $action))
+        {
+            $request  = $this->slim->getContainer()->get('request');
+            $response = $this->slim->getContainer()->get('response');
+
+            return $this->{$action}($request, $response, $params);
+        }
+
+        return false;
     }
 
 }
