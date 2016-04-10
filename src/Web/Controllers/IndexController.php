c0979ac139150b800c5d010fb96d29d268dad800
tyurd <tyurderi@yahoo.de>
2016-04-10 13:52:14 +0200

    Index action

--- src/Web/Controllers/IndexController.php
+++ src/Web/Controllers/IndexController.php
@@ -9,7 +9,18 @@ use TM\Config\Web\ControllerAbstract;
 class IndexController extends ControllerAbstract
 {
 
-    public function indexAction()
+    /**
+     * Index action
+     *
+     * @pattern /
+     * @method  GET
+     *
+     * @param $request  Request
+     * @param $response Response
+     *
+     * @return string
+     */
+    public function indexAction(Request $request, Response $response)
     {
         return $this->view->pick('index/index')->finish();
     }
