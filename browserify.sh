e042011661f1d20afcad89ebcc4e6ae23b77f61a
Tommy <meier@rhinos-media.de>
2016-04-14 16:50:49 +0200

    minimizing bundle

--- browserify.sh
+++ browserify.sh
@@ -6,3 +6,8 @@ browserify \
     $path/custom.js \
     -o $path/custom.bundle.js \
     --noparse=*.*
+
+if [ "$1" == "--minify" ]; then
+    minifyjs -mi $path/custom.bundle.js -o $path/custom.min.js
+    mv $path/custom.min.js $path/custom.bundle.js
+fi
