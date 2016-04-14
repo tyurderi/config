6facdb2e6b706cc65aa586e97b9dd0810a8c48b2
Tommy <meier@rhinos-media.de>
2016-04-14 16:12:28 +0200

    move configuration filter logic to particular file

--- web/resources/js/custom.js
+++ web/resources/js/custom.js
@@ -1,50 +1,7 @@
 (function($, window, require, undefined) {
     "use strict";
 
-    var $configurationList  = $('.configuration--list .list--items'),
-        $configurationForm  = $('.configuration-filter--form form'),
-        $formInput          = $configurationForm.find('input'),
-        timeout             = null,
-        $configurationCount = $('.configuration--count');
-
-    $formInput.on('change input', function() {
-        if(timeout)
-        {
-            clearTimeout(timeout);
-        }
-
-        timeout = setTimeout(function() {
-            $configurationForm.submit();
-        }, 250);
-    });
-
-    $configurationForm.on('submit', function(e) {
-        e.preventDefault();
-
-        var url  = $(this).attr('action'),
-            data = $(this).serialize();
-
-        $.post(url, data, function(response) {
-            $configurationList.empty();
-
-            response.data.forEach(function(item) {
-                var $item = $('<div />', {
-                    'data-configId': item.id,
-                    'html': item.label,
-                    'class': 'item'
-                });
-
-                $item.appendTo($configurationList);
-                $item.on('click', function() {
-                    console.log('Loading configuration with id %d...', item.id);
-                });
-            });
-
-            $configurationCount.html(response.count)
-        });
-
-    });
-
-    $configurationForm.submit();
+    // initialize configuration filter
+    require('configuration/filter')();
 
 })(jQuery, window, require);
