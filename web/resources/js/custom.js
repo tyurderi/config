f460ab20f9abbe87bb1d51c1522349a9d39976ab
Tommy <meier@rhinos-media.de>
2016-04-20 10:02:35 +0200

    add eventhandler

--- web/resources/js/custom.js
+++ web/resources/js/custom.js
@@ -1,7 +1,8 @@
 (function($, window, require, undefined) {
     "use strict";
 
-    window.Class = require('class-js2');
+    window.Class         = require('class-js2');
+    window.EventAbstract = require('local/event_abstract');
 
     // initialize configuration filter
     require('local/configuration/filter')();
