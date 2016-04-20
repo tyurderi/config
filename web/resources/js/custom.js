c9ad931d7d60446ddbbf5ac73c0a7a376b2be246
Tommy <meier@rhinos-media.de>
2016-04-20 10:52:03 +0200

    caching class objects whether instanciated often

--- web/resources/js/custom.js
+++ web/resources/js/custom.js
@@ -3,6 +3,9 @@
 
     window.Class         = require('class-js2');
     window.EventAbstract = require('local/event_abstract');
+    window.JCache        = require('local/jcache');
+
+    JCache.set('form_builder', new (require('local/form_builder')));
 
     // initialize configuration filter
     require('local/configuration/filter')();
